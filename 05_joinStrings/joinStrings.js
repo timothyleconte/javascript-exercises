59b27c684d86337c15ae505a76eb81471094c31d
timothyleconte <marktbooher@outlook.com>
2025-06-18 00:11:19 -0400

    Finish first set of exercises 1-5

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,6 +9,16 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+
+let age = thisYear - birthYear;
+let fullName = firstName + " " + lastName;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
+
 
 
 
